d580098d9bfbb39ed2fa583aa0da7636f5468398
Sebastian Nordby <Sebastian.nordby@apotek1.no>
2024-08-28 12:47:24 +0200

    Trigger build

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -73,7 +73,6 @@ export function PrivacyPage() {
     );
 }
 
-
 // export function PrivacyPage() {
 //     return (
 //         <div className="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-lg mt-10">
