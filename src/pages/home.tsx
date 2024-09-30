37207805d416bc4d9ea11c12431c7eee3e7288f2
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2024-09-30 15:54:39 +0200

    Brushing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,7 @@ import {
   GITHUB_LINK,
   LINKED_IN_LINK,
 } from './constants';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const consultingApps = [
   {
@@ -21,78 +21,100 @@ const consultingApps = [
 export default function HomePage() {
   const { t } = useTranslation();
   const [menuOpen, setMenuOpen] = useState(false); // State for responsive menu
+  const [navBg, setNavBg] = useState('bg-transparent'); // State for nav background
+  const visibleNavClass = 'bg-white dark:bg-gray-900 shadow-md';
+
+  // Handle scroll event to change navbar background
+  useEffect(() => {
+    const mainElement = document.querySelector('main') as HTMLDivElement;
+    const handleScroll = () => {
+      if (mainElement.scrollTop > 200) {
+        setNavBg('bg-white dark:bg-gray-900 shadow-md'); // Change to white after scroll
+      } else {
+        setNavBg('bg-transparent'); // Keep transparent at the top
+      }
+    };
+
+    mainElement!.addEventListener('scroll', handleScroll);
+
+    // Cleanup scroll event listener
+    return () => {
+      mainElement!.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <div>
-      <div className="hero-bg min-h-[500px] dark:bg-black">
-        <header>
-          <nav className="p-4 flex flex-wrap justify-between items-center max-w-7xl mx-auto">
-            <div className="flex items-center justify-between w-full md:w-auto">
-              <Link to={'/'}>
-                <h1 className="text-xl font-bold text-gray-800 dark:text-white">
-                  Nordby Solutions
-                </h1>
-              </Link>
-              <button
-                className="md:hidden text-gray-800 dark:text-white"
-                onClick={() => setMenuOpen(!menuOpen)}
-                aria-label="Toggle menu"
-              >
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16m-7 6h7"
-                  />
-                </svg>
-              </button>
-            </div>
-
-            <div
-              className={`w-full md:flex md:items-center md:w-auto ${menuOpen ? 'block' : 'hidden'}`}
+      <header
+        className={`fixed w-full transition-colors duration-300 ${menuOpen ? visibleNavClass : navBg}`}
+      >
+        <nav className="p-4 pr-6 flex flex-wrap justify-between items-center max-w-7xl mx-auto">
+          <div className="flex items-center justify-between w-full md:w-auto">
+            <Link to={'/'}>
+              <h1 className="text-xl font-bold text-gray-800 dark:text-white">
+                Nordby Solutions
+              </h1>
+            </Link>
+            <button
+              className="md:hidden text-gray-800 dark:text-white"
+              onClick={() => setMenuOpen(!menuOpen)}
+              aria-label="Toggle menu"
             >
-              <div className="flex flex-col md:flex-row gap-4 items-center mt-4 md:mt-0">
-                <Link
-                  className="hover:underline text-gray-800 dark:text-gray-300"
-                  to={CV_URL}
-                >
-                  {t('common.cv')}
-                </Link>
-                <Link
-                  className="hover:underline text-gray-800 dark:text-gray-300"
-                  to={APP_PORTFOLIO_URL}
-                >
-                  {t('common.applications')}
-                </Link>
-                <a
-                  className="hover:underline text-gray-800 dark:text-gray-300"
-                  href={LINKED_IN_LINK}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-                <a
-                  className="hover:underline text-gray-800 dark:text-gray-300"
-                  href={GITHUB_LINK}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Github
-                </a>
-              </div>
-            </div>
-          </nav>
-        </header>
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </button>
+          </div>
 
-        <div className="flex flex-col items-center justify-center mt-12">
+          <div
+            className={`w-full md:flex md:items-center md:w-auto ${menuOpen ? 'block' : 'hidden'}`}
+          >
+            <div className="flex flex-col md:flex-row gap-4 items-center mt-4 md:mt-0">
+              <Link
+                className="hover:underline text-gray-800 dark:text-gray-300"
+                to={CV_URL}
+              >
+                {t('common.cv')}
+              </Link>
+              <Link
+                className="hover:underline text-gray-800 dark:text-gray-300"
+                to={APP_PORTFOLIO_URL}
+              >
+                {t('common.applications')}
+              </Link>
+              <a
+                className="hover:underline text-gray-800 dark:text-gray-300"
+                href={LINKED_IN_LINK}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+              <a
+                className="hover:underline text-gray-800 dark:text-gray-300"
+                href={GITHUB_LINK}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div className="hero-bg min-h-[585px] dark:bg-black">
+        <div className="flex flex-col items-center justify-center px-3 pt-24">
           <div className="text-center max-w-xl">
             <h1 className="text-4xl font-bold mb-4 text-gray-800 dark:text-white">
               Fra Enkle Nettsider til Skreddersydde Løsninger
